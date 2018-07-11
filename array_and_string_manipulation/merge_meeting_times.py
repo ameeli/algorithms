86abf526fe1db68c408b52bf6391e84b7a6a51a1
Amee Li <ameejli@gmail.com>
2018-07-10 18:07:25 -0700

    Completed merge meeting times.

---
+++ array_and_string_manipulation/merge_meeting_times.py
@@ -0,0 +1,14 @@
+def merge_ranges(meetings):
+
+    # Merge meeting ranges
+    meetings.sort()
+    all_meetings = [meetings[0]]
+    
+    for start, end in meetings:
+        if start - all_meetings[-1][1] < 1: 
+            if end > all_meetings[-1][1]:
+                all_meetings[-1] = (all_meetings[-1][0], end)
+        else:
+            all_meetings.append((start, end))
+
+    return all_meetings
