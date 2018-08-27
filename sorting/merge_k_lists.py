abd966fe4b9c70a058376d16d83c870b9c77e77b
Amee Li <ameejli@gmail.com>
2018-08-27 14:18:48 -0700

    Completed merge k sorted lists.

---
+++ sorting/merge_k_lists.py
@@ -0,0 +1,24 @@
+import heapq
+
+def merge_k_lists(lsts):
+    """Given k sorted lists, merge into one sorted list."""
+    k = len(lsts)
+    indices = [0 for _ in xrange(k)]
+    merged = []
+    compare_nums = []
+
+    for i, lst in enumerate(lsts):
+        heapq.heappush(compare_nums, (lst[0], i))
+
+    while compare_nums:
+        min_num, min_idx = heapq.heappop(compare_nums)
+        merged.append(min_num)
+        indices[min_idx] += 1
+
+        if indices[min_idx] < len(lsts[min_idx]):
+            heapq.heappush(compare_nums, (lsts[min_idx][indices[min_idx]], min_idx))
+
+    return merged
+
+
+print merge_k_lists([[1, 5, 10], [2, 3, 7], [4, 8, 9]])
