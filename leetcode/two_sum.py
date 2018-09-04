ef1a97fddb11a6f35cb98c372acee75059be3de7
Amee Li <ameejli@gmail.com>
2018-09-04 10:49:08 -0700

    Completed two sum.

---
+++ leetcode/two_sum.py
@@ -0,0 +1,19 @@
+"""
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+"""
+
+def twoSum(self, nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    
+    to_add = {}
+    
+    for i in range(len(nums)):
+        remainder = target - nums[i]
+        if remainder in to_add:
+            return to_add[remainder], i
+        to_add[nums[i]] = i
