4b5aedc4c76f3ed669d500b59cffd0a8dc40b54d
ameeli <ameejli@gmail.com>
2020-04-13 20:07:45 -0700

    Update merge k sorted lists

--- sorting/merge_k_lists.py
+++ sorting/merge_k_lists.py
@@ -1,26 +1,28 @@
 import heapq
 
+
 def merge_k_lists(lsts):
     """Given a list of k sorted lists, merge into one sorted list."""
-    k = len(lsts)
-    indices = [0 for i in xrange(k)]
-    merged = []
+    lst_idx = [0] * len(lsts)
     compare_nums = []
+    merged = []
 
     for i, lst in enumerate(lsts):
         heapq.heappush(compare_nums, (lst[0], i))
 
     while compare_nums:
-        min_num, min_idx = heapq.heappop(compare_nums)
-        merged.append(min_num)
-        indices[min_idx] += 1
+        min_val, min_val_lst_idx = heapq.heappop(compare_nums)
+        merged.append(min_val)
+        lst_idx[min_val_lst_idx] += 1
 
-        if indices[min_idx] < len(lsts[min_idx]):
-            lst_number = lsts[min_idx]
-            item_index = indices[min_idx]
-            heapq.heappush(compare_nums, (lst_number[item_index], min_idx))
+        if lst_idx[min_val_lst_idx] < len(lsts[min_val_lst_idx]):
+            add_from_lst = lsts[min_val_lst_idx]
+            item_idx = lst_idx[min_val_lst_idx]
+            heapq.heappush(compare_nums, (
+                add_from_lst[item_idx], min_val_lst_idx)
+            )
 
     return merged
 
 
-print merge_k_lists([[1, 5, 10], [2, 3, 7], [4, 8, 9]])
+print(merge_k_lists([[2, 3, 7], [1, 5, 10], [4, 8, 9], [6]]))
