55bed8d22534c1617e4ce94948321b291a7b802f
ameeli <ameejli@gmail.com>
2020-07-13 11:39:53 -0700

    Improved fibonacci solution space with memoization

--- interview_prep/fibonacci.py
+++ interview_prep/fibonacci.py
@@ -12,12 +12,31 @@ def compute_fibonacci(n):
     return compute_fibonacci(n - 1) + compute_fibonacci(n - 2)
 
 
+def compute_finonacci_memo(n, memo=None):
+    if not memo:
+        memo = [0] * (n + 1)
+
+    if n == 0 or n == 1:
+        return n
+
+    if memo[n] == 0:
+        memo[n] = compute_finonacci_memo(
+            n - 1, memo) + compute_finonacci_memo(
+            n - 2, memo)
+
+    return memo[n]
+
+
 class TestFibonacci(unittest.TestCase):
     test_cases = [(0, 0), (1, 1), (13, 233), (25, 75025)]
 
     def test_compute_fibonacci(self):
         for n, ans in self.test_cases:
             self.assertEqual(compute_fibonacci(n), ans)
+
+    def test_compute_fibonacci_memo(self):
+        for n, ans in self.test_cases:
+            self.assertEqual(compute_finonacci_memo(n), ans)
 
 
 if __name__ == '__main__':
