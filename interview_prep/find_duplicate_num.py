071d9d77defc8284908b5bdff3b7d055d4fcdd13
ameeli <ameejli@gmail.com>
2020-07-28 10:39:37 -0700

    Solve in O(n lg n) time O(1) space

---
+++ interview_prep/find_duplicate_num.py
@@ -0,0 +1,37 @@
+import unittest
+
+
+def find_repeat(numbers):
+    numbers.sort()
+
+    for i, num in enumerate(numbers, 1):
+        if num == numbers[i]:
+            return num
+
+    raise Exception('No duplicates in this list.')
+
+
+class Test(unittest.TestCase):
+
+    def test_just_the_repeated_number(self):
+        actual = find_repeat([1, 1])
+        expected = 1
+        self.assertEqual(actual, expected)
+
+    def test_short_list(self):
+        actual = find_repeat([1, 2, 3, 2])
+        expected = 2
+        self.assertEqual(actual, expected)
+
+    def test_medium_list(self):
+        actual = find_repeat([1, 2, 5, 5, 5, 5])
+        expected = 5
+        self.assertEqual(actual, expected)
+
+    def test_long_list(self):
+        actual = find_repeat([4, 1, 4, 8, 3, 2, 7, 6, 5])
+        expected = 4
+        self.assertEqual(actual, expected)
+
+
+unittest.main(verbosity=2)
